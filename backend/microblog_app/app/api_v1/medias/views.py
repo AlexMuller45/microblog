64562ea43093bebe16196fe1ada06de8c9279f28
Aleksey Melnikov <mas-chel@mail.ru>
2023-10-13 20:05:07 +0500

    hotfix upload file

--- backend/microblog_app/app/api_v1/medias/views.py
+++ backend/microblog_app/app/api_v1/medias/views.py
@@ -53,14 +53,14 @@ def get_image(image_name: str) -> FileResponse:
     response_model=MediaAdd,
 )
 async def add_media(
-    in_file: UploadFile = File(...),
+    file: UploadFile = File(...),
     session: AsyncSession = Depends(db_helper.scoped_session_dependency),
 ):
     """
     Добавление имени файла в БД, присвоение ID, сохранение файла на сервер
 
     Args:
-        in_file (UploadFile): Входящий файл.
+        file (UploadFile): Входящий файл.
         session (AsyncSession): Асинхронный сеанс SQLAlchemy.
 
     Returns:
@@ -71,21 +71,22 @@ async def add_media(
 
     """
 
-    if not in_file:
+    if not file:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST, detail="No file sent"
         )
-    if not imghdr.what(in_file.file):
+    if not imghdr.what(file.file):
         raise HTTPException(
             status_code=status.HTTP_415_UNSUPPORTED_MEDIA_TYPE,
             detail="The file is not an image",
         )
 
-    media_id: int = await crud.add_media(session=session, file_name=in_file.filename)
-    file_path = f"{settings.media_path}/{settings.filename.format(media_id=media_id, in_file_name=in_file.filename)}"
+    media_id: int = await crud.add_media(session=session, file_name=file.filename)
+    file_path = f"{settings.media_path}/{settings.filename.format(media_id=media_id, in_file_name=file.filename)}"
 
     async with aiofiles.open(file_path, "wb") as out_file:
-        # await out_file.write(in_file.read())
-        shutil.copyfileobj(in_file.file, out_file)
+        content = await file.read()
+        await out_file.write(content)
+        # shutil.copyfileobj(in_file.file, out_file)
 
     return {"result": True, "media_id": media_id}
