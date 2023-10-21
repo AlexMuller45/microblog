8bc6698b469ec62726b155f509bbc62aa5176de1
Aleksey Melnikov <mas-chel@mail.ru>
2023-10-21 15:30:03 +0500

    added test endpoint Medias

--- backend/microblog_app/tests/test_endpoint_like.py
+++ backend/microblog_app/tests/test_endpoint_like.py
@@ -14,7 +14,6 @@ class TestEndpointsLike:
 
     async def test_endpoint_add_like(self, ac: AsyncClient, test_session: AsyncSession):
         tweet_id: int = await get_tweet_id(test_session)
-        print("* " * 5, tweet_id)
         response = await ac.post(f"{self.endpoint}{tweet_id}/like", headers=self.auth)
         assert response.status_code == 201
 
@@ -25,7 +24,6 @@ class TestEndpointsLike:
         self, ac: AsyncClient, test_session: AsyncSession
     ):
         tweet_id = await get_tweet_id_through_like(test_session)
-        print("*  " * 5, tweet_id)
         response = await ac.delete(f"{self.endpoint}{tweet_id}/like", headers=self.auth)
         assert response.status_code == 200
         assert response.json() == {"result": True}
---
+++ backend/microblog_app/tests/test_endpoint_media.py
@@ -0,0 +1,48 @@
+import os
+
+import aiofiles
+from httpx import AsyncClient
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from backend.microblog_app.tests.conftest import (
+    test_session,
+    ac,
+)
+from core.config import settings
+from core.models import Media
+
+
+class TestEndpointsMedias:
+    endpoint: str = "/api/medias/"
+    auth: dict[str] = {"api-key": "test"}
+    media_dir = settings.media_path
+
+    async def test_endpoint_get_media(self, ac: AsyncClient):
+        os.makedirs(self.media_dir, exist_ok=True)
+        async with aiofiles.open(
+            os.path.join(self.media_dir, "test_image.jpg"), "w"
+        ) as f:
+            await f.write("test content")
+
+        response = await ac.get(f"{self.endpoint}test_image.jpg", headers=self.auth)
+        assert response.status_code == 200
+
+        os.remove(os.path.join(self.media_dir, "test_image.jpg"))
+
+    async def test_endpoint_add_media(
+        self, ac: AsyncClient, test_session: AsyncSession
+    ):
+        files = {"file": ("temp_file.jpg", open("temp_file.jpg", "rb"))}
+
+        response = await ac.post(self.endpoint, files=files)
+        assert response.status_code == 201
+
+        stmt = select(Media).where(Media.id == response.json()["media_id"])
+        result = await test_session.execute(stmt)
+        _media = result.scalar()
+
+        assert _media is not None
+
+        await test_session.delete(_media)
+        await test_session.commit()
